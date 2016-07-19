8be808abed553e497431d02a9827e4e7064e588e
goldishs <simchagoldish@gmail.com>
2016-07-18 20:16:48 -0400

    create wildfire.r

---
+++ wildfire.r
@@ -0,0 +1,30 @@
+####################################################
+#Math 3800
+#Seth Goldish, Ely Biggs, Taylor Saintable
+#
+#Flood Model     Main program    Version 0.01
+#
+#
+#
+#
+#
+#
+#
+####################################################
+
+# LIBRARIES AND FUNCTIONS
+####################################################
+
+### http://gis.stackexchange.com/questions/20955/how-to-generate-a-random-but-believable-digital-elevation-model
+#if raster has not yet been installed, run:
+#packages.install("raster")
+require(raster)
+source("Helperfunctions.R")
+#set.seed(100) # makes the random not random 
+
+
+
+
+####################################################
+#   START OF MAIN PROGRAM
+####################################################
