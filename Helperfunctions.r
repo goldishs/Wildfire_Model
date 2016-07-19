9eb28fa80561522cb286c227052b853021e09c39
goldishs <simchagoldish@gmail.com>
2016-07-18 20:17:32 -0400

    create Helperfunctions.r

---
+++ Helperfunctions.r
@@ -0,0 +1,14 @@
+####################################################
+#Math 3800
+#Seth Goldish, Ely Biggs, Taylor Saintable
+#
+#Wildfire Model:   Helperfunctions.R
+#
+#       Functions:
+# 
+# 
+# 
+#   
+#
+#
+####################################################
